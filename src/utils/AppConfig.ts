e16cd6bcf7d98ac80b20aa827ab3301f26c1eb64
Lee Del Rosario <86918855+leernbee@users.noreply.github.com>
2022-03-10 09:50:37 +0800

    Update AppConfig.ts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'PasaJobs',
+  site_name: 'For educational purpose only',
+  title: 'For educational purpose only',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
